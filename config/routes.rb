1e04322e822cfa8ee76b12296f3a595bc90dc176
Dimitri Namour <dimitri.namour@gmail.com>
2020-02-20 17:35:09 +0100

    exo 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/delete'
+  resources :cocktails, only: [:index, :create, :new, :show] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
